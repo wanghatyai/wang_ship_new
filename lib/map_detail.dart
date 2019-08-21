a61249019eab9aa9a79a1fff60aaf620bbc70713
wangAppPC <wanghatyai@gmail.com>
2019-08-21 16:43:37 +0700

    init mapDetailPage

---
+++ lib/map_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapDetailPage extends StatefulWidget {
+  @override
+  _MapDetailPageState createState() => _MapDetailPageState();
+}
+
+class _MapDetailPageState extends State<MapDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
